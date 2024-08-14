3b7c434be0b83dbb72d2717814723440a65e5dd1
Nazarbek <matyoqubovn123@gmail.com>
2024-08-14 09:29:31 +0500

    'updated'

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+import requests
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    r=requests.get("https://nbu.uz/en/exchange-rates/json/")
+    a= json.loads(r.content)
+    b=a[-1]
+    return HttpResponse(f"hozirgi paytda dollir kursi {b['nbu_buy_price'][0:5]} so'mga teng")
